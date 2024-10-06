05c0ad960a5b93bcf8dbe6c11a5f6f6e377efde0
ThanuraSilva <thanurasilva1989@gmail.com>
2024-10-06 12:30:45 +0530

    Error correct - dao

--- src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/dao/NoteDao.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/dao/NoteDao.java
@@ -1,6 +1,6 @@
 package lk.ijse.gdse.aad68.NoteCollectorV2.dao;
 
-import lk.ijse.gdse.aad68.notetaker.entity.NoteEntity;
+import lk.ijse.gdse.aad68.NoteCollectorV2.entity.NoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/dao/UserDao.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/dao/UserDao.java
@@ -1,6 +1,6 @@
 package lk.ijse.gdse.aad68.NoteCollectorV2.dao;
 
-import lk.ijse.gdse.aad68.notetaker.entity.UserEntity;
+import lk.ijse.gdse.aad68.NoteCollectorV2.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
